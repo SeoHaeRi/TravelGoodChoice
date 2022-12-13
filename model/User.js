441648793795d4c3e2ee867f7f83aee44ce743cf
SeoHaeRi <jackie100425@naver.com>
2022-12-13 16:13:22 +0900

    feat: sequelize 초기 설정

---
+++ model/User.js
@@ -0,0 +1,32 @@
+const User = (Sequelize, DataTypes) => {
+    return Sequelize.define(
+        // table name
+        "user",
+        {
+            id: {
+                type: DataTypes.STRING(20), // varchar(10)
+                allowNull: false,
+                primaryKey: true
+            },
+            pw: {
+                type: DataTypes.STRING(20),
+                allowNull: false,
+            },
+            name: {
+                type: DataTypes.STRING(10),
+                allowNull: false,
+            },
+            profile_img: {
+                type: DataTypes.BLOP('long'),
+                allowNull: true,
+            }
+        },
+        {
+            tablename: "travel",
+            freezeTableName: true,
+            timestamps: false
+        }
+    );
+}
+
+module.exports = User;

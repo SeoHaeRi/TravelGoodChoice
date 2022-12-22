fb25956d3a163864e333e20a91a0ece3b2e3eec8
SeoHaeRi <jackie100425@naver.com>
2022-12-22 15:30:29 +0900

    feat: 카카오 로그아웃 구현, session 수정

--- controller/Cuser.js
+++ controller/Cuser.js
@@ -5,8 +5,10 @@ const axios = require('axios');
 const kakao = {
   clientID: process.env.KAKAO_clientID,
   redirectUri: process.env.KAKAO_redirectUri,
-  clientSecret: process.env.KAKAO_clientSecret
+  clientSecret: process.env.KAKAO_clientSecret,
+  logoutRedirectUri: process.env.KAKAO_logoutRedirectUri
 };
+let accessToken;
 
 exports.view_login = (req, res) => {
   res.render("login")
@@ -63,6 +65,18 @@ exports.logout = (req, res) => {
     res.send(true);
   })
 }
+exports.view_kakaoLogout = (req, res) => {
+  const kakaoLogout = `https://kauth.kakao.com/oauth/logout?client_id=${kakao.clientID}&logout_redirect_uri=${kakao.logoutRedirectUri}`;
+  res.redirect(kakaoLogout);
+}
+
+exports.kakaoLogout = (req, res) => {
+  console.log("logout: ", req);
+  req.session.destroy((err) => {
+    if (err) throw err;
+    res.redirect('/');
+  })
+}
 
 exports.view_kakaoLogin = (req, res) => {
   const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code`;
@@ -71,7 +85,7 @@ exports.view_kakaoLogin = (req, res) => {
 
 exports.kakaoLogin = async (req, res) => {
   //access토큰을 받기 위한 코드
-  let token = await axios({
+  await axios({
     method: 'POST',
     url: 'https://kauth.kakao.com/oauth/token',
     headers: {
@@ -86,13 +100,13 @@ exports.kakaoLogin = async (req, res) => {
     })
   }).then((res) => {
     console.log("토큰 정보 : ", res.data);
+    accessToken = res.data.access_token;
     return userInfo(res.data);
   }).then((user) => {
     console.log("user : ", user);
     console.log(req.data);
 
-
-    // database에 있는 user면 그냥 바로 seswsion, 없으면 create
+    // database에 있는 user면 그냥 바로 session, 없으면 create
     let data = {
       id: user.kakao_account.email,
       pw: '',
@@ -104,13 +118,13 @@ exports.kakaoLogin = async (req, res) => {
     User.findOne({ where: { [Op.or]: [{ id: data.id }, { name: data.name }] } })
       .then((result) => {
         if (result) {
-          req.session.user = user.kakao_account.email;
+          req.session.kakao = user.kakao_account.email;
           res.redirect('/');
         }
         else {
           User.create(data)
             .then(() => {
-              req.session.user = user.kakao_account.email;
+              req.session.kakao = user.kakao_account.email;
               res.redirect('/');
             })
         }

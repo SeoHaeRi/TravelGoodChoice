9b2481899957eb48ae2e52f3c30b2f3e86a5a239
MinsunCho-dev <cms081222@gmail.com>
2022-12-27 05:21:35 +0900

    feat: 로그인 & 회원가입 수정

--- model/User.js
+++ model/User.js
@@ -20,12 +20,23 @@ const User = (Sequelize, DataTypes) => {
         type: DataTypes.STRING(200),
         allowNull: true,
       },
+      hint: {
+        type: DataTypes.STRING(10),
+        allowNull: false, 
+      },
+      hint_answer: {
+        type: DataTypes.STRING(50),
+        allowNull: false, 
+      },
       social_type: {
         type: DataTypes.STRING(10),
         allowNull: true,
       }
-    },
+  },
+  // table option
     {
+      charset: "utf8", // 한국어 설정
+      collate: "utf8_general_ci", // 한국어 설정
       tablename: "travel",
       freezeTableName: true,
       timestamps: false
--- routes/user.js
+++ routes/user.js
@@ -4,19 +4,20 @@ const multer = require("multer");
 const path = require('path');
 const router = express.Router();
 
+/* user 프로필 업로드 */
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, done) {
-      done(null, 'static/upload_img');
+      done(null, 'static/upload_img');  
     },
     filename(req, file, done) {
       console.log("filename: ", req.body);
       const ext = path.extname(file.originalname);
       const filename = req.body.name + ext;
       done(null, filename);
-    }
-  })
-})
+    },
+  }),
+});
 
 router.get('/login', controller.view_login)
 router.post('/signup', upload.single("userfile"), controller.signup);
@@ -26,5 +27,8 @@ router.get('/login/kakao', controller.view_kakaoLogin);
 router.get('/kakao/finish', controller.kakaoLogin);
 router.get('/logout/kakao', controller.view_kakaoLogout);
 router.get('/kakao/logout', controller.kakaoLogout);
+// router.get('/find',controller.find_index); 
+// router.post('/modify',controller.pw_modify); 
+// router.post('/update', controller.pw_update); 
 
 module.exports = router;

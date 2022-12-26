bc4b6c130b4c9bbc0df1e8953004c3d2be5f29e1
SeoHaeRi <jackie100425@naver.com>
2022-12-27 01:50:37 +0900

    feat: 댓글 목록 띄우기

--- controller/Ccomment.js
+++ controller/Ccomment.js
@@ -1,23 +1,23 @@
-const { Comment, Post, sequelize } = require("../model");
+const { Comment, sequelize } = require("../model");
 
 exports.addComment = async (req, res) => {
-    let data;
-    if (req.session.user) {
-        data = {
-            post_id: req.body.post_id,
-            name: req.session.user.name,
-            content: req.body.content,
-            img: req.session.user.profile_img,
-        }
-    } else {
-        data = {
-            post_id: req.body.post_id,
-            name: req.session.kakao.name,
-            content: req.body.content,
-            img: req.session.kakao.profile_img,
-        }
+  let data;
+  if (req.session.user) {
+    data = {
+      post_id: req.body.post_id,
+      name: req.session.user.name,
+      content: req.body.content,
+      img: req.session.user.profile_img,
     }
+  } else {
+    data = {
+      post_id: req.body.post_id,
+      name: req.session.kakao.name,
+      content: req.body.content,
+      img: req.session.kakao.profile_img,
+    }
+  }
 
-    let result = await Comment.create(data);
-    res.send(result)
+  let result = await Comment.create(data);
+  res.send(result)
 }
